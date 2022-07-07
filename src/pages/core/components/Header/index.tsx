033d59d0baed42247ac268ee7e54562abf0685f1
solebellsBEACH <lucassxxavier@gmail.com>
2022-07-07 17:28:36 -0300

    criando estrutura

---
+++ src/pages/core/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from './styles'
+
+export const Header = () => {
+    return (
+        <Container>
+            Header
+        </Container>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { Container } from './styles'
+
+const Home: NextPage = () => {
+  return (
+    <Container>
+      akjdsadfhsafhgsfhkas
+    </Container>
+  )
+}
+
+export default Home
