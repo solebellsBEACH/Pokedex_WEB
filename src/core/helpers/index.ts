d86c22470d39dbbebb59037190a6614eedde7854
solebellsBEACH <lucassxxavier@gmail.com>
2022-07-07 18:11:20 -0300

    adicionando as colors

---
+++ src/core/helpers/index.ts
@@ -0,0 +1,17 @@
+export const colors = () => {
+    return {
+        white: 'white',
+        blue1: '#2E6EB5',
+        blue2: '#1554F6',
+        blue3: '#4A7DFF',
+        gray1: '#3333330F',
+        gray2: '#D8D8D8',
+        gray3: '#5E5E5EB2',
+        gray4: '#6B6060',
+        gray5: '#676767',
+        gray6: '#3F3F3F',
+        gray7: '#5E5D5D',
+        black: '#5E5D5D',
+        red1: '#FF1D1D'
+    }
+}
