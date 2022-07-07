2705e92ce458e0d0008f47bf3dd2133da3e7fbdd
solebellsBEACH <lucassxxavier@gmail.com>
2022-07-07 18:08:48 -0300

    adicionando os assets

---
+++ src/assets/index.ts
@@ -0,0 +1,18 @@
+import BackgroundLogIn from './background-login.jpg'
+import LogoPokemon from './logo-pokemon.svg'
+import LogoConfig from './config-logo.svg'
+import ExitIcon from './exit-icon.svg'
+import ExitCircleIcon from './exit-circle-icon.svg'
+import PokemonImage from './pokemon-image.svg'
+import ArrowLeftIcon from './arrow-left-icon.svg'
+import BigPokemon from './big-pokemon.svg'
+export {
+    BigPokemon,
+    ArrowLeftIcon,
+    BackgroundLogIn,
+    LogoPokemon,
+    LogoConfig,
+    ExitIcon,
+    PokemonImage,
+    ExitCircleIcon
+}
