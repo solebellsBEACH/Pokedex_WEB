c3a12602d80fd140c37ca210f9f4f9c0d7c6e8f6
solebellsBEACH <lucassxxavier@gmail.com>
2022-07-30 10:19:24 -0300

    deixando carrossel responsivo

--- src/core/components/Carousel/index.tsx
+++ src/core/components/Carousel/index.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import { useMediaQuery } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
 import { CarouselIMG02, CarouselIMG03, CarouselIMG04, CarouselIMG05, CarouselIMG01 } from '../../../assets/carousel'
 import { Container, StyledImage, StyledCarousel } from './styles'
 
 
 export const Carousel = () => {
+
+    const [slideToShow, setSlideToShow] = useState(1);
+    const [isLargerThan1400] = useMediaQuery('(min-width: 1400px)');
+    const [isLargerThan700] = useMediaQuery('(min-width: 700px)');
+
+    useEffect(() => {
+       console.log(slideToShow)
+        if ([isLargerThan1400, isLargerThan700].toString() == [true, true].toString()) {
+            setSlideToShow(3)
+        }
+        if ([isLargerThan1400, isLargerThan700].toString() == [false, true].toString()) {
+            setSlideToShow(2)
+        }
+        if ([isLargerThan1400, isLargerThan700].toString() == [false, false].toString()) {
+            setSlideToShow(1)
+        }
+    }, [isLargerThan1400, isLargerThan700])
+
     return (
         <Container>
             <StyledCarousel
                 wrapAround={true}
                 animation='zoom'
-                slidesToShow={3}
+                slidesToShow={slideToShow}
                 cellAlign='center'
                 withoutControls
                 autoplay
