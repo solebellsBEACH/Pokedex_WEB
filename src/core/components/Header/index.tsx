814997060b505f938102c9295ceb649e78c9b395
solebellsBEACH <lucassxxavier@gmail.com>
2022-08-09 15:46:41 -0300

    estilizando header

--- src/core/components/Header/index.tsx
+++ src/core/components/Header/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 
-import { Container, ContentImage, ImageLogoPokemon } from './styles'
+import { CarrinhoButton, Container, Content, ContentImage, FavoriteButton, ImageLogoPokemon } from './styles'
 import { LogoPokemon } from '../../../assets';
 import { useRouter } from 'next/router';
+import { useMediaQuery } from '@chakra-ui/react';
+import { BsCart3, BsFillHeartFill} from 'react-icons/bs'
 
 interface IHeaderProps {
 
 }
 
+
 export const Header = () => {
     const router = useRouter();
+    const [isLargerThan660] = useMediaQuery('(min-width: 660px)');
+
     return (
         <Container>
             <ContentImage
@@ -17,8 +22,17 @@ export const Header = () => {
                     router.push('/')
                 }}
             >
-                <ImageLogoPokemon height='100%' src={LogoPokemon} />
+
+                <ImageLogoPokemon src={LogoPokemon} />
             </ContentImage>
+            {!isLargerThan660 ? <></> : <Content>
+                <FavoriteButton>
+                <BsFillHeartFill size={20} />
+                </FavoriteButton>
+                <CarrinhoButton>
+                    <BsCart3 size={23} />
+                </CarrinhoButton>
+            </Content>}
         </Container>
     )
 }
--- src/core/components/Header/styles.ts
+++ src/core/components/Header/styles.ts
@@ -1,19 +1,62 @@
 import styled from 'styled-components'
 import Image from 'next/image'
-import { colors } from '../../helpers'
+import { colors } from '../../helpers';
 
-export const Container = styled.div`
-width:100%;
-margin-top:1.5vh;
-display:flex;
-justify-content:center;
-align-items:center;
+export const Container = styled.nav`
+    width:100%;
+    display:flex;
+    margin-bottom:30px ;
+    flex-direction:row;
+    align-items:center;
+    padding: 0px 40px;
+    background: ${colors().background};
+    justify-content:space-between;
+    border-bottom:1px ${colors().gray1} solid;
 
 `
+export const Content = styled.div`
+    width:80% ;
+    margin-left:20%;
+    height:100%;
+    display:flex;
+    flex-direction:row;
+    align-items:center;
+    justify-content:flex-end;
+`;
 
 export const ContentImage = styled.div`
+    width:120px;
+    display:flex;
+    margin:5px 0;
+    @media (max-width:660px){
+        justify-content:center;
+        align-items:center;
+        width:100%;
+    }
+    padding:2px ;
+`;
 
-
-`
 export const ImageLogoPokemon = styled(Image)`
+`;
+
+export const CarrinhoButton = styled.button`
+     margin:0 0 0 2vw;   
+    width: 40px;
+    height: 40px;
+    border-radius:5px;
+    display: flex;
+    align-items:center;
+    justify-content:center;
+    color:${colors().gray7};
+    &:hover{
+        color:${colors().gray6};
+    }
+`
+
+export const FavoriteButton = styled.button`
+    color:${colors().gray7};
+    &:hover{
+        color:${colors().gray6};
+    }
+    margin:0 0 0 2vw;
 `
