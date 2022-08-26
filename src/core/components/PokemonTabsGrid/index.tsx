0ff57302a7cca6bbb6ab3beacddc7797449a52ec
solebellsBEACH <lucassxxavier@gmail.com>
2022-08-26 10:36:32 -0300

    addicioando loadings

--- src/core/components/PokemonTabsGrid/index.tsx
+++ src/core/components/PokemonTabsGrid/index.tsx
@@ -5,6 +5,7 @@ import { PokemonGrid } from '../../../pageComplements/home/components';
 import { IHomeDuckInitialState } from '../../interfaces';
 import { Container, TabSelectorContainer, TabSelectorText, ContentTop, ContentBottom } from './styles'
 import { Creators as HomeActions } from '../../../core/store/ducks/home'
+import { Spinner } from '@chakra-ui/react';
 interface IPokemonTabsGrid {
     filtersActiveds: string[];
     setActiveTab: React.Dispatch<React.SetStateAction<string>>;
@@ -45,7 +46,7 @@ export const PokemonTabsGrid = ({ filtersActiveds, setActiveTab, activeTab }: IP
                 {filtersActiveds.map((item, index) => <TabSelector key={index + item} label={item} />)}
             </ContentTop>
             <ContentBottom>
-                <PokemonGrid filtersActiveds pokemons={homeData.pokemons} />
+                {homeData.pokemons==null||homeData.pokemons ==undefined?<Spinner size='lg'/>:<PokemonGrid filtersActiveds pokemons={homeData.pokemons} />}
             </ContentBottom>
         </Container>
     )
--- src/pageComplements/home/components/PokemonGrid/index.tsx
+++ src/pageComplements/home/components/PokemonGrid/index.tsx
@@ -1,6 +1,7 @@
-import { Box, WrapItem } from '@chakra-ui/react'
+import { Box, Spinner, WrapItem } from '@chakra-ui/react'
 import React from 'react'
-import { IPokemon, IPokemonRequest } from '../../../../core/interfaces'
+import { useSelector } from 'react-redux'
+import { IHomeDuckInitialState, IPokemon, IPokemonRequest } from '../../../../core/interfaces'
 import { PokemonItem } from '../PokemonItem'
 import { Container, Grid } from './styles'
 
@@ -9,7 +10,7 @@ interface IPokemonGridProps {
     filtersActiveds?: boolean
 }
 export const PokemonGrid = ({ pokemons, filtersActiveds }: IPokemonGridProps) => {
-
+    const homeData = useSelector((state: { home: IHomeDuckInitialState }) => state.home)
     const PokemonMap = (results: IPokemon[]) => {
         if (results !== undefined && pokemons != null) {
 
@@ -52,8 +53,8 @@ export const PokemonGrid = ({ pokemons, filtersActiveds }: IPokemonGridProps) =>
                 spacingY='30px'
                 justify='center'
             >
-
-                {pokemons !== null ? PokemonMap(pokemons.data) : <></>}
+                {homeData.error ? <h1>Nenhum Pokemon encontrado</h1> : <>
+                    {pokemons !== null ? PokemonMap(pokemons.data) : <Spinner size='lg' />}</>}
             </Grid>
         </Container>
     )
